3ee70824242a231b14e85acbc847ca3e6995aa68
G Vishal <42919948+VISH01AL@users.noreply.github.com>
2023-11-14 18:55:18 -0500

    Update tcp_traceroute.py

--- tcp_traceroute.py
+++ tcp_traceroute.py
@@ -1,5 +1,6 @@
 import socket
-import struct
+from scapy.all import IP, TCP
+
 
 def get_my_ip():
     try:
@@ -18,49 +19,23 @@ def get_ip_address(domain):
         return ip_address
     except socket.gaierror:
         return None
-def create_syn_packet(source_ip, source_port, dest_ip, dest_port):
-    # IP header
-    ip_version = 4
-    ip_ihl = 5
-    ip_tos = 0
-    ip_total_length = 0
-    ip_id = 54321
-    ip_flags = 0
-    ip_fragment_offset = 0
-    ip_ttl = 1
-    ip_protocol = socket.IPPROTO_TCP
-    ip_checksum = 0
-    ip_source = socket.inet_aton(source_ip)
-    ip_dest = socket.inet_aton(dest_ip)
+    
+def create_syn_ack_packet(destination_ip, destination_port):
+    # Create an IP packet with the destination IP address
+    ip_packet = IP(dst=destination_ip)
 
-    ip_header = struct.pack('!BBHHHBBH4s4s', (ip_version << 4) + ip_ihl, ip_tos, ip_total_length, ip_id, (ip_flags << 13) + ip_fragment_offset, ip_ttl, ip_protocol, ip_checksum, ip_source, ip_dest)
+    # Create a TCP SYN-ACK packet with the destination port
+    tcp_packet = TCP(dport=destination_port, flags="SA", seq=1000, ack=5000)
 
-    # TCP header
-    tcp_source = source_port
-    tcp_dest = dest_port
-    tcp_seq = 12345
-    tcp_ack_seq = 0
-    tcp_offset_res = (5 << 4)
-    tcp_flags = 0x02  # SYN flag
-    tcp_window = socket.htons(5840)
-    tcp_checksum = 0
-    tcp_urg_ptr = 0
+    # Combine the IP and TCP packets
+    syn_ack_packet = ip_packet / tcp_packet
 
-    tcp_header = struct.pack('!HHLLBBHHH', tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags, tcp_window, tcp_checksum, tcp_urg_ptr)
+    # Display the packet details
+    print("TCP SYN-ACK Packet:")
+    print(syn_ack_packet.summary())
 
-    # Pseudo header for TCP checksum calculation
-    pseudo_header = struct.pack('!4s4sBBH', socket.inet_aton(source_ip), socket.inet_aton(dest_ip), 0, socket.IPPROTO_TCP, len(tcp_header))
-    checksum_data = pseudo_header + tcp_header
-    temp=struct.unpack('!HHHH', checksum_data)
-    tcp_checksum = socket.htons(socket.htons(sum(temp)))
-
-    # Update the TCP header with the calculated checksum
-    tcp_header = struct.pack('!HHLLBBHHH', tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags, tcp_window, tcp_checksum, tcp_urg_ptr)
-
-    # Combine everything into a packet
-    packet = ip_header + tcp_header
-    print(packet)
-    return packet
+    # Return the packet
+    return syn_ack_packet
 
 def send_syn_packet(packet, dest_ip):
     raw_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
@@ -73,7 +48,7 @@ def send_syn_packet(packet, dest_ip):
 
 
 def traceroute(source_ip, source_port, destination_ip, destination_port):
-    syn_packet = create_syn_packet(source_ip, source_port, destination_ip, destination_port)
+    syn_packet = create_syn_ack_packet(destination_ip, destination_port)
     print(send_syn_packet(syn_packet, destination_ip))
     return []
 
