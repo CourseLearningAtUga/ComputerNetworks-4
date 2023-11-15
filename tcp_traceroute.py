ded0c3717af78dc31622610518a641c7f2494bdc
VISH01AL <g.vish1al@gmail.com>
2023-11-14 21:10:57 -0500

    started working on project and added the basic traceroute

--- tcp_traceroute.py
+++ tcp_traceroute.py
@@ -1,76 +1,122 @@
 import socket
-from scapy.all import IP, TCP
+import struct
+import argparse
+import time
+from scapy.all import IP, TCP,ICMP
+
+def process_icmp_packet(response):
+    # Check if the packet has an ICMP layer
+    if ICMP in response:
+        # Extract ICMP layer
+        icmp_layer = response[ICMP]
+
+        # Print information about the ICMP packet
+        print(f"ICMP Type: {icmp_layer.type}, Code: {icmp_layer.code}")
+        print(f"ID: {icmp_layer.id}, Seq: {icmp_layer.seq}")
+
+        # Access the payload (data) of the ICMP packet
+        payload_data = icmp_layer.payload
+        print(f"Payload Data: {payload_data}")
 
 
-def get_my_ip():
-    try:
-        # Create a socket and connect to an external server (Google's DNS server)
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
-        local_ip = s.getsockname()[0]
-        s.close()
-        return local_ip
-    except socket.error as e:
-        print(f"Error: {e}")
-        return None
-def get_ip_address(domain):
-    try:
-        ip_address = socket.gethostbyname(domain)
-        return ip_address
-    except socket.gaierror:
-        return None
+def send_tcp_syn_packet(destination_ip, ttl, dst_port):
     
-def create_syn_ack_packet(destination_ip, destination_port,ttl):
-    # Create an IP packet with the destination IP address
-    ip_packet = IP(dst=destination_ip,ttl=ttl)
+    # Create a raw socket
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
 
-    # Create a TCP SYN-ACK packet with the destination port
-    tcp_packet = TCP(dport=destination_port, flags="SA", seq=1000, ack=5000)
+    # Set the TTL in the IP header
+    tcp_socket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
 
-    # Combine the IP and TCP packets
-    syn_ack_packet = ip_packet / tcp_packet
-
-    # Display the packet details
-    print("TCP SYN-ACK Packet:")
-    print(syn_ack_packet.summary())
-    print(bytes(syn_ack_packet))
-    # Return the packet
-    return bytes(syn_ack_packet)
-
-def send_syn_packet(packet, dest_ip):
-    raw_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-
-    # Set socket options to enable IP header included in the packet
-    raw_socket.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-
+    packet = IP(dst=destination_ip, ttl=ttl) / TCP(dport=dst_port, flags="S")
+    print("sentpacket+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++start")
     # Send the packet
-    raw_socket.sendto(packet, (dest_ip, 0))
-    data,addr=raw_socket.recvfrom(2048)
-    print(data)
-    print(addr)
-    return raw_socket
-
-
-def traceroute(source_ip, source_port, destination_ip, destination_port):
-    ttl=2
-    syn_packet = create_syn_ack_packet(destination_ip, destination_port,ttl)
+    print(packet)
+    print(bytes(packet))
+    print("sentpacket+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++end")
+    tcp_socket.sendto(bytes(packet), (destination_ip, dst_port))
     
-    send_syn_packet(syn_packet, destination_ip)
-    return []
+    # Record the time the packet was sent
+    send_time = time.time()
 
-def main(targetdomain,maxhops,dst_port):
-    source_ip = get_my_ip()
-    destination_ip=get_ip_address(targetdomain)
-    source_port=1234
-    destination_port=80
-    print(source_ip)
-    print(destination_ip)
-    traceroute(source_ip, source_port, destination_ip, destination_port)
+    return tcp_socket, send_time
+
+def receive_icmp():
+    # Set a timeout on the socket
+    icmp_raw_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    try:
+        # Receive the TCP SYN-ACK packet
+        while True:
+            # Receive an ICMP packet
+            icmp_raw_socket.settimeout(1)
+            packet, addr = icmp_raw_socket.recvfrom(1024)
+            
+            recv_time=time.time()
+
+            print("=====================================2")
+            print(packet)
+            print(addr)
+            print("=====================================3")
+            icmp_raw_socket.close()
+            return addr,recv_time
+    except socket.timeout:
+        return None,0
 
 
+def process_icmp_packet(response):
+    # Check if the packet has an ICMP layer
+    if ICMP in response:
+        # Extract ICMP layer
+        icmp_layer = response[ICMP]
+
+        # Print information about the ICMP packet
+        print(f"ICMP Type: {icmp_layer.type}, Code: {icmp_layer.code}")
+        print(f"ID: {icmp_layer.id}, Seq: {icmp_layer.seq}")
+
+        # Access the payload (data) of the ICMP packet
+        payload_data = icmp_layer.payload
+        print(f"Payload Data: {payload_data}")
+
+def tcp_traceroute(tracerouteoutput,curriter,target, max_hops=5, dst_port=80):
+    print(f"TCP Traceroute to {target}, {max_hops} hops max, TCP SYN to port {dst_port}")
+    tracerouteoutput.append([])
+    for ttl in range(1, max_hops + 1):
+        # Send TCP SYN packet
+        
+        tcp_socket, send_time = send_tcp_syn_packet(target, ttl, dst_port)
+
+        # Receive TCP SYN-ACK packet
+        addr,receive_time = receive_icmp()
+        print("time=============================================start")
+        # print(send_time)
+        # print(receive_time)
+        print(receive_time-send_time)
+        print("time=============================================end")
+        # Close the TCP socket
+        tcp_socket.close()
+
+        if addr:
+            # Calculate round-trip time
+            round_trip_time = (receive_time - send_time) * 1000  # in milliseconds
+            print(f"{ttl}\t{addr}\t{round_trip_time:.3f} ms")
+            tracerouteoutput[curriter].append([addr,round_trip_time])      
+        else:
+            tracerouteoutput[curriter].append([addr,round_trip_time])      
+            print(f"{ttl}\t*")
+
+        # Check if we reached the destination
+        if addr == target:
+            break
 
 if __name__ == "__main__":
-    targetdomain="8.8.8.8"
-    maxhops=30
-    dst_port=80
-    main(targetdomain,maxhops,dst_port)
+    parser = argparse.ArgumentParser(description="TCP Traceroute")
+    parser.add_argument("-m", type=int, default=30, help="Max hops to probe (default = 30)")
+    parser.add_argument("-p", type=int, default=80, help="TCP destination port (default = 80)")
+    parser.add_argument("-t", type=str, required=True, help="Target domain or IP")
+    args = parser.parse_args()
+    tracerouteoutput=[]
+    
+    tcp_traceroute(tracerouteoutput,0,args.t, max_hops=args.m, dst_port=args.p)
+    
+    # print(tracerouteoutput)
+    for x in tracerouteoutput[0]:
+        print(x)
